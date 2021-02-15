49e7e2b616b3d3e122c0998390dfbdb319ee6b3c
jorge <black.spade.666@gmail.com>
2021-02-15 13:58:07 -0500

    footer, best movies

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import MoviesSlider from "../components/MoviesSlider";
 import { API_KEY, API_URL } from "../utils/constants";
 import MoviesList from "../components/MoviesList";
 import { Row, Col } from "antd";
+import Footer from "../components/Footer";
 
 export default function Home() {
   const newmoviesUrl = `${API_URL}movie/now_playing?api_key=${API_KEY}&language=es-Es&page=1`;
@@ -11,6 +12,9 @@ export default function Home() {
 
   const popularMoviesUrl = `${API_URL}movie/popular?api_key=${API_KEY}&language=es-Es&page=1`;
   const popularMovies = useFetch(popularMoviesUrl);
+
+  const bestMoviesUrl = `${API_URL}movie/top_rated?api_key=${API_KEY}&language=es-Es&page=1`;
+  const bestMovies = useFetch(bestMoviesUrl);
 
   return (
     <>
@@ -22,8 +26,14 @@ export default function Home() {
             title="Peliculas Populares"
           ></MoviesList>
         </Col>
-        <Col span={12}>..</Col>
+        <Col span={12}>
+          <MoviesList
+            movies={bestMovies}
+            title="Mejores Peliculas"
+          ></MoviesList>
+        </Col>
       </Row>
+      <Footer />
     </>
   );
 }
